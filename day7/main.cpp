17a5dbddd8f188828a6fc0005bad4528c9fd05d6
Oliver Hookins <ohookins@gmail.com>
2020-12-08 22:49:03 +1100

    Add day 7 part 2.

--- day7/main.cpp
+++ day7/main.cpp
@@ -13,15 +13,15 @@ const std::string TARGET_BAG = "shiny gold";
 struct Node
 {
     std::string Name;
-    std::vector<Node*> Children;
+    std::vector<std::pair<int, Node*>> Children;
     std::vector<Node*> Parents;
 };
 
 // store all nodes for later lookup when constructing the graph
 std::map<std::string, Node*> NodeMap;
 
-// map of name to childrennames to resolve once we have everything parsed
-std::map<std::string, std::vector<std::string>> UnresolvedNodeMap;
+// map of name to children counts and names to resolve once we have everything parsed
+std::map<std::string, std::vector<std::pair<int, std::string>>> UnresolvedNodeMap;
 
 // add a single leaf node
 // has no dependencies so it is easy
@@ -34,18 +34,18 @@ void addLeafNode(std::string Name)
 
 // Returns true if the node was successfully added to the NodeMap,
 // false otherwise.
-bool addBranchNode(std::string Name, std::vector<std::string> ChildNames, bool AddToUnresolved = true)
+bool addBranchNode(std::string Name, std::vector<std::pair<int, std::string>> Children, bool AddToUnresolved = true)
 {
     // check that all children are known, otherwise put this node on the
     // UnresolvedNodeList "queue" to be added to the graph later.
-    for (auto ChildName : ChildNames)
+    for (const auto& [Count, ChildName] : Children)
     {
         if (NodeMap.count(ChildName) != 1)
         {
             // Child wasn't found, can't put this node and its children
             // into the node list yet.
             // We skip this if we are resolving everything.
-            if (AddToUnresolved) UnresolvedNodeMap.insert({Name, ChildNames});
+            if (AddToUnresolved) UnresolvedNodeMap.insert({Name, Children});
             return false;
         }
     }
@@ -55,13 +55,13 @@ bool addBranchNode(std::string Name, std::vector<std::string> ChildNames, bool A
     // each child to facilitate navigation of the graph.
     Node* CurrentNode = new Node();
     CurrentNode->Name = Name;
-    for (auto ChildName : ChildNames)
+    for (const auto& [Count, ChildName] : Children)
     {
         Node* ChildNode = NodeMap[ChildName];
 
         // add up and down relationship pointers
         ChildNode->Parents.push_back(CurrentNode);
-        CurrentNode->Children.push_back(ChildNode);
+        CurrentNode->Children.push_back({Count, ChildNode});
     }
 
     // finally add this node to the list of resolved nodes
@@ -89,17 +89,17 @@ void parseLine(std::string Line)
     std::string Name = cmatch[1].str();
     std::string Suffix = cmatch[2].str();
 
-    std::regex BagsRE("(\\d+ (\\w+ \\w+) bags?[,. ]+)");
+    std::regex BagsRE("((\\d+) (\\w+ \\w+) bags?[,. ]+)");
     std::smatch smatch;
-    std::vector<std::string> ChildNames;
+    std::vector<std::pair<int, std::string>> Children;
     while (std::regex_search(Suffix, smatch, BagsRE))
     {
-        // 2 is the submatch of the two word name of the child bag
-        ChildNames.push_back(smatch[2].str());
+        // 2 & 3 are the submatches of the bag count and two word name of the child bag
+        Children.push_back({std::stoi(smatch[2].str()), smatch[3].str()});
         Suffix = smatch.suffix();
     }
 
-    addBranchNode(Name, ChildNames);
+    addBranchNode(Name, Children);
 }
 
 void readInput()
@@ -164,11 +164,34 @@ void findContainingBagCount(Node* StartingNode)
     }
 }
 
+// Starting from a given bag name, do a search to find all child
+// nodes and return the total count, multiplied by how many bags
+// of each child type this current bag can hold.
+int calculateChildBagCount(Node* StartingNode)
+{
+    // include this bag as it will be needed by the caller's calculation
+    int bagCount = 1;
+
+    for (const auto& [ChildCount, ChildNode] : StartingNode->Children)
+    {
+        bagCount += calculateChildBagCount(ChildNode) * ChildCount;
+    }
+
+    return bagCount;
+}
+
 int main(int argc, char** argv)
 {
     readInput();
     resolveBranchNodes();
-    findContainingBagCount(NodeMap[TARGET_BAG]);
 
+    // Find and count all unique ancestors ("containing bags") of this bag.
+    findContainingBagCount(NodeMap[TARGET_BAG]);
     std::cout << TARGET_BAG << " bag has " << VisitedNodeNames.size() << " containing bags." << std::endl;
+
+    // Find and count all of the child bags of this bag.
+    // This one is different as we need to multiply all of the bag counts up the chain.
+    int count = calculateChildBagCount(NodeMap[TARGET_BAG]);
+    count--; // don't count the starting bag itself!
+    std::cout << TARGET_BAG << " bag contains " << count << " other bags." << std::endl;
 }
