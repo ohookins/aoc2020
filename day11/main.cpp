0675515ac372ac9720fe0ec62f7b55600563b75f
Oliver Hookins <ohookins@gmail.com>
2020-12-15 22:50:53 +1100

    Add day 11 part 2, another massacre.

--- day11/main.cpp
+++ day11/main.cpp
@@ -9,7 +9,12 @@ const std::string INPUT_FILENAME = "input.txt";
 
 // Add two cells in either dimension to make boundary checking easier.
 // I'm having trouble figuring out if the input file is actually 98x98 or 99x99.
-const int DIM = 100;
+const int DIM = 101;
+
+enum class Direction
+{
+    N, NE, E, SE, S, SW, W, NW
+};
 
 class Layout
 {
@@ -26,6 +31,7 @@ public:
         NewStorage = &StorageB;
     }
 
+    // Only count the neighbours in the immediately surrounding 8 positions
     int CountNeighbours(int x, int y)
     {
         // Indexes in the current storage to the surrounding 8
@@ -50,6 +56,24 @@ public:
             if ((*CurrentStorage)[Index] == '#') Neighbours++;
         }
         return Neighbours;
+    }
+
+    // Count the visible neighbours in the surrounding 8 directions, irrespective of
+    // distance. If an empty chair is visible first, don't examine further.
+    int CountVisibleNeighbours(int x, int y)
+    {
+        std::vector<Direction> Directions = {
+            Direction::N, Direction::NE, Direction::E, Direction::SE,
+            Direction::S, Direction::SW, Direction::W, Direction::NW
+        };
+
+        int Count = 0;
+        for (auto d : Directions)
+        {
+            if (SeeNeighboursInDirection(x, y, d)) Count++;
+        }
+
+        return Count;
     }
 
     // convenience functions
@@ -131,6 +155,64 @@ private:
 
         return (*CurrentStorage)[y*DIM + x];
     }
+
+    bool SeeNeighboursInDirection(const int x, const int y, const Direction LookDirection)
+    {
+        // "velocities" of travel
+        // in retrospect, this is a horrible way of doing it.
+        int xVel = 0, yVel = 0;
+        switch (LookDirection)
+        {
+            case Direction::N:
+                yVel = -1;
+                break;
+            case Direction::NW:
+                yVel = -1;
+                xVel = -1;
+                break; 
+            case Direction::NE:
+                yVel = -1;
+                xVel = 1;
+                break;
+            case Direction::S:
+                yVel = 1;
+                break;
+            case Direction::SW:
+                yVel = 1;
+                xVel = -1;
+                break;
+            case Direction::SE:
+                yVel = 1;
+                xVel = 1;
+                break;
+            case Direction::W:
+                xVel = -1;
+                break;
+            case Direction::E:
+                xVel = 1;
+                break;
+        }
+
+        // iterate through layout in the view direction until we fall off
+        // or see an empty chair or neighbour
+        int currentX = x, currentY = y;
+        while (true)
+        {
+            currentX += xVel;
+            currentY += yVel;
+
+            // bounds check
+            if (currentX <= 0 || currentX >= DIM - 1 || currentY <= 0 || currentY >= DIM - 1) return false;
+
+            char VisibleObject = Get(currentX, currentY);
+
+            // see an empty chair, we don't care about what is beyond it
+            if (VisibleObject == 'L') return false;
+        
+            // see a neighbour
+            if (VisibleObject == '#') return true;
+        }
+    }
 };
 
 void ReadInput(std::function<void(std::string)> function)
@@ -152,8 +234,10 @@ void ReadInput(std::function<void(std::string)> function)
     }
 }
 
-int RunSimulation(Layout* Ferry)
+int RunSimulation(Layout* Ferry, bool RelaxedRuleset = false)
 {
+    int MaxNeighbourTolerance = RelaxedRuleset ? 5 : 4;
+
     bool HaveMadeChanges = false;
 
     for (int y = 1; y < DIM; y++)
@@ -163,7 +247,9 @@ int RunSimulation(Layout* Ferry)
             // floor, do nothing
             if (Ferry->IsFloor(x, y)) continue;
 
-            int Neighbours = Ferry->CountNeighbours(x, y);
+            int Neighbours = 0;
+            if (RelaxedRuleset) Neighbours = Ferry->CountVisibleNeighbours(x, y);
+            else Neighbours = Ferry->CountNeighbours(x, y);
 
             // Empty seat with no neighbours - fill
             if (!Ferry->IsFilled(x, y) && Neighbours == 0)
@@ -172,8 +258,8 @@ int RunSimulation(Layout* Ferry)
                 HaveMadeChanges = true;
             }
 
-            // Filled with four or more neighbours - empty
-            if (Ferry->IsFilled(x, y) && Neighbours >= 4)
+            // Filled with X or more neighbours - empty
+            if (Ferry->IsFilled(x, y) && Neighbours >= MaxNeighbourTolerance)
             {
                 Ferry->Empty(x, y);
                 HaveMadeChanges = true;
@@ -184,29 +270,41 @@ int RunSimulation(Layout* Ferry)
     }
 
     Ferry->Commit();
-    if (HaveMadeChanges) return RunSimulation(Ferry) + 1;
+    if (HaveMadeChanges) return RunSimulation(Ferry, RelaxedRuleset) + 1;
     return 1;
 }
 
 int main(int argc, char** argv)
 {
-    // Storage
-    Layout Ferry;
+    // Two temporary storages for the layout since I'm lazy and the memory
+    // is mutated after the simulation (could copy but I closed off that
+    // path early).
+    Layout FerryPart1;
+    Layout FerryPart2;
 
     // Fill all cells except one line around the outside
     int row = 0;
-    ReadInput([&Ferry, &row](std::string Line) {
+    ReadInput([&FerryPart1, &FerryPart2, &row](std::string Line) {
         for (int col = 0; col < Line.size(); col++)
         {
-            Ferry.Set(col+1, row+1, Line[col]);
+            FerryPart1.Set(col+1, row+1, Line[col]);
+            FerryPart2.Set(col+1, row+1, Line[col]);
         }
         row++;
     });
 
     // switch storage pointers since we used the Set interface to
     // it above, which only writes to the new storage location.
-    Ferry.Commit();
-    int Iterations = RunSimulation(&Ferry);
-    std::cout << "Simulation stabilised after " << Iterations << " iterations." << std::endl;
-    std::cout << Ferry.OccupiedSeatsCount() << " seats were occupied." << std::endl;
+    FerryPart1.Commit();
+    FerryPart2.Commit();
+
+    // Part1
+    int Iterations = RunSimulation(&FerryPart1);
+    std::cout << "(Part1) Simulation stabilised after " << Iterations << " iterations." << std::endl;
+    std::cout << FerryPart1.OccupiedSeatsCount() << " seats were occupied." << std::endl;
+
+    // Part2
+    Iterations = RunSimulation(&FerryPart2, true);
+    std::cout << std::endl << "(Part2) Simulation stabilised after " << Iterations << " iterations." << std::endl;
+    std::cout << FerryPart2.OccupiedSeatsCount() << " seats were occupied." << std::endl;
 }
